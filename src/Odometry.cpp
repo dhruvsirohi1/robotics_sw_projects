00567425bcad4fcdb67d8511d56841c9616d863c
Dhruv <dsirohi98@gmail.com>
2025-08-04 12:35:54 -0700

    implement odometry.cpp

--- src/Odometry.cpp
+++ src/Odometry.cpp
@@ -19,4 +19,47 @@ namespace robot_localization {
         }
     }
 
+    void RobotOdometry::pushEncoderData(double delta_s_left, double delta_s_right) {
+        std::lock_guard<std::mutex> lock(queue_mutex_);
+        queue_.emplace(delta_s_left, delta_s_right);
+        queue_cv_.notify_one();
+    }
+
+    Pose2d RobotOdometry::getPose() const {
+        std::lock_guard<std::mutex> lock(pose_mutex_);
+        return pose_;
+    }
+
+    void RobotOdometry::updateThreadLoop() {
+        while (running_) {
+            std::unique_lock<std::mutex> lock(queue_mutex_);
+            queue_cv_.wait(lock, [&] { return !queue_.empty() || !running_; });
+
+            while (!queue_.empty()) {
+                auto [delta_s_left, delta_s_right] = queue_.front();
+                queue_.pop();
+                lock.unlock();
+
+                double delta_s = (delta_s_left + delta_s_right) / 2.0;
+                double delta_theta = (delta_s_right - delta_s_left) / wheel_base_;
+
+                double theta_mid;
+                {
+                    std::lock_guard<std::mutex> pose_lock(pose_mutex_);
+                    theta_mid = pose_.theta_ + delta_theta / 2.0;
+
+                    pose_.x_ += delta_s * std::cos(theta_mid);
+                    pose_.y_ += delta_s * std::sin(theta_mid);
+                    pose_.theta_ += delta_theta;
+
+                    // Normalize theta to [-pi, pi]
+                    if (pose_.theta_ > M_PI)
+                        pose_.theta_ -= 2 * M_PI;
+                    else if (pose_.theta_ < -M_PI)
+                        pose_.theta_ += 2 * M_PI;
+                }
+                lock.lock();
+            }
+        }
+    }
 }
