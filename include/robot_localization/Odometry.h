6c61046c1ad1582a2f289433c5c81afe579e2e70
Dhruv <dsirohi98@gmail.com>
2025-08-04 12:09:54 -0700

    setup odometry header to have thread safety

--- include/robot_localization/Odometry.h
+++ include/robot_localization/Odometry.h
@@ -6,16 +6,35 @@
 #define ODOMETRY_H
 
 #include "Pose2d.h"
+#include <queue>
+#include <mutex>
+#include <thread>
+#include <atomic>
+#include <condition_variable>
 
-class RobotOdometry {
-public:
-    explicit RobotOdometry(double wheel_base);
+namespace robot_localization {
+    class RobotOdometry {
+    public:
+        explicit RobotOdometry(double wheel_base);
+        ~RobotOdometry();
 
-    void update(double delta_s_left, double delta_s_right);
-    [[nodiscard]] Pose2d getPose() const;
+        void pushEncoderData(double delta_s_left, double delta_s_right);
+        [[nodiscard]] Pose2d getPose() const;
 
-private:
-    double wheel_base_;
-    Pose2d pose_;
-};
+    private:
+        void updateThreadLoop();
+
+        // Robot state
+        double wheel_base_;
+        mutable std::mutex pose_mutex_;
+        Pose2d pose_;
+
+        std::thread thread_;
+        std::atomic<bool> running_;
+
+        std::queue<std::pair<double, double>> queue_;
+        std::mutex queue_mutex_;
+        std::condition_variable queue_cv_;
+    };
+} // namespace robot_localization
 #endif //ODOMETRY_H
