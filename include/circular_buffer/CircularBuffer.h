faf9622b72466a3f39e979841eb90a7f0d8d59d7
Dhruv <dsirohi98@gmail.com>
2025-08-04 22:46:29 -0700

    add a circular buffer library

---
+++ include/circular_buffer/CircularBuffer.h
@@ -0,0 +1,70 @@
+//
+// Created by Dhruv Sirohi on 8/4/25.
+//
+
+#ifndef CIRCULARBUFFER_H
+#define CIRCULARBUFFER_H
+
+#include <vector>
+
+namespace circular_buffer {
+template <typename T>
+class CircularBuffer {
+    int capacity_;
+    int size_;
+    std::vector<T> buffer_;
+    int head_;
+    int tail_;
+    bool is_full_;
+
+public:
+
+    CircularBuffer(int capacity):
+    capacity_(capacity),
+    size_(capacity),
+    buffer_(capacity),
+    head_(0),
+    tail_(0),
+    is_full_(false) {
+    };
+
+    void push(T element) {
+        buffer_[head_] = element;
+        head_ = (head_ + 1) % capacity_;
+
+        if (is_full_) {
+            tail_ = (tail_ + 1) % capacity_;
+        }
+
+        size_ = std::min(size_ + 1, capacity_);
+
+        is_full_ = tail_ == head_;
+    }
+
+    T pop() {
+        if (size_ == 0) return nullptr;
+
+        T element = buffer_[tail_];
+        tail_ = (tail_ + 1) % capacity_;
+        size_--;
+        is_full_ = false;
+        return element;
+    }
+
+    const T& front() const {
+        if (size_ == 0) return nullptr;
+        return buffer_[head_];
+    }
+
+    size_t size() const {
+        return size_;
+    }
+
+    void clear() {
+        head_ = tail_;
+        is_full_ = false;
+        size_ = 0;
+    }
+};
+}
+#endif //CIRCULARBUFFER_H
