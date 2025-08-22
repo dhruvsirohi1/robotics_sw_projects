5a02bd5ef405bb9aa783f69bbeab6a3d2fa2dad5
Dhruv <dsirohi98@gmail.com>
2025-08-22 09:46:25 -0700

    another memory pool

--- include/memory_pool/memory_pool.h
+++ include/memory_pool/memory_pool.h
@@ -8,32 +8,30 @@
 
 namespace memory_pool {
 
-template<typename T, std::size_t N>
+template <typename T, std::size_t N>
 class MemoryPool {
     static_assert(N > 0, "N must be greater than 0");
 
-    constexpr static auto S = sizeof(T) > sizeof(void*) ? sizeof(T) : sizeof(void*);
-    constexpr static auto A = alignof(T) > alignof(void*) ? alignof(T) : alignof(void*);
+    static constexpr auto A = alignof(T) > alignof(void*) ? alignof(T) : alignof(void*);
+    static constexpr auto S = sizeof(T) > sizeof(void*) ? sizeof(T) : sizeof(void*);
 
-    constexpr static auto BLOCK_SIZE = (S + A - 1) / A * A;
+    static constexpr auto BLOCK_SIZE = (S + A - 1) / A * A;
 
-    alignas(A) std::array<std::byte, N * BLOCK_SIZE> data_;
+    std::array<std::byte, N * BLOCK_SIZE> data_;
     void* free_ptr_;
 
 public:
 
-    MemoryPool() : free_ptr_(nullptr) {
-        for (std::size_t i = 0; i < N - 1; ++i) {
+    MemoryPool() {
+        printf("MemoryPool constructor\n");
+        for (int i = 0; i < N - 1; i++) {
             std::byte* curr = data_.data() + i * BLOCK_SIZE;
             std::byte* next = curr + BLOCK_SIZE;
             std::memcpy(curr, &next, sizeof(void*));
         }
-
-        // Store nullptr in the last block
         void* nullp = nullptr;
-        std::memcpy(data_.data() + (N - 1) * BLOCK_SIZE, &nullp, sizeof(void*));
+        std::memcpy(data_.data() + (N-1) * BLOCK_SIZE, &nullp, sizeof(void*));
 
-        // Make free_ptr_ point to the first free block
         free_ptr_ = data_.data();
     }
 
@@ -41,6 +39,7 @@ public:
         if (free_ptr_ == nullptr) {
             throw std::bad_alloc();
         }
+
         T* ptr = reinterpret_cast<T*>(free_ptr_);
         std::memcpy(&free_ptr_, free_ptr_, sizeof(void*));
         return ptr;
